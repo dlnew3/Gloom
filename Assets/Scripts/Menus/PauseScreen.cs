6a1dc3aa9a18afa08c4640a9b2dbc36c5d254cb5
Shane <shanewilsonpro@gmail.com>
2020-05-02 15:29:50 -0700

    adding pause screen

---
+++ Assets/Scripts/Menus/PauseScreen.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScreen : MonoBehaviour
+{
+    public GameObject pauseScreenUI; //[SerializeField]
+    public bool isPaused; //[SerializeField]
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                isPaused = true;
+                pauseScreenUI.SetActive(true);
+                Time.timeScale = 1f;
+            }
+        }
+
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+        pauseScreenUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void QuitToMain()
+    {
+        SceneManager.LoadScene("MainMenuScene");
+    }
+}
